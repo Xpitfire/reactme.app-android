b23290132e818b34f4af0b690182682eedf57d82
Marius-Constantin Dinu <Marius-Constantin Dinu>
2017-02-16 00:04:44 +0100

    Added additional package definitions

--- generate-swagger-classes.bat
+++ generate-swagger-classes.bat
@@ -2,4 +2,4 @@ rem This command requires a working swagger-codegen compilation see:
 rem https://github.com/swagger-api/swagger-codegen
 rem and set environment variable pointing to %SWAGGER_HOME%, which represents
 rem the git checkout directory!
-java -jar %SWAGGER_HOME%\modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate --api-package com.dork.app.react.api -i http://localhost:38806/swagger/v1/swagger.json -l java -o .\swagger
+java -jar %SWAGGER_HOME%\modules\swagger-codegen-cli\target\swagger-codegen-cli.jar generate --api-package com.dork.app.react.api --invoker-package com.dork.app.react.api.invoker --model-package com.dork.app.react.api.model -v -i http://localhost:38806/swagger/v1/swagger.json -l java -o .\swagger
